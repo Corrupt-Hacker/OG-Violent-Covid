e67445983456df065d266b4535c01b73237ee6e8
Jack Geraghty <97810268+ViolentCovid@users.noreply.github.com>
2023-03-15 10:50:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,25 +5,23 @@ const currentNews = document.querySelector('.currentNews');
 
 //functions
 function insultWd() {
-    alert('Your Insanly Dumb');
-    alert('Nobody Looks Worse Than YOU!!!');
+    alert('Your Tragicly Dumb To The Point Where Got Regrets Making You');
+    alert('Your Father Jumped Of A Cliff To Get Away From You');
     alert('Everybody Hates You And Only You');
-    alert('I Dont Know Anybody I Hate More That You');
+    alert('Jump Off A Cliff Because You Being Dead Would Make The World A Better Place');
     alert('I Think Your Best Bet Is To Hop In The Dumpster And Be Taken To The Nearest Land Fill Where You Belong');
-    alert('Make Sure To Fill Out The Form At The Bottom At The Page To Stay Notifyed');
 }
 function complementWd() {
     alert("Your Breath Doesn't stink today");
     alert("You BUZZCUT Looks Better Than Ever");
     alert("You Dont Have That Big Of An Ego. (I Can't Say Enless Your Cade Anymore Because Cade Now Uses My Site)");
     alert('You Have Tons Of Friends Because Your The Best');
-    alert('Make Sure To Fill Out The Form At The Bottom At The Page To Stay Notifyed');
 }
 function newsWd() {
-   alert('Update 3.0 Will Occure Before March!');
-   alert('What Do You Call A Fish With No Eyes? A Fsh');
-   alert('Get It, I Know You Laughed And You Can"t Denie It');
-   alert('Make Sure To Fill Out The Form At The Bottom At The Page To Stay Notifyed');
+   alert('Updates Are Going To Slow Down Thanks To Me Being In Hot Water With School I.T.');
+   alert('I Can"t Predict When The Next Update Will Be');
+   alert('Again DO NOT Give This Site To Anyone. Snitches Have Started Getting My Site Such As Sammy. If Sammy Happens To Be On My Site Reading This...');
+   alert('Sammy Go Fuck Yourself');
 }
 
 //Calling All Functions & Event Listeners
